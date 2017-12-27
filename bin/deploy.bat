5a0cc8f0a9f8d10a6202d8ebf976c3337492db48
Olaf Hartong <olafhartong@Olafs-MacBook-Pro.local>
2017-12-27 17:15:56 +0000

    Splunk install location detection

--- bin/deploy.bat
+++ bin/deploy.bat
@@ -1,10 +1,13 @@
 echo off
 >> c:\windows\sysmon.log (
+FOR /F "delims=" %%i IN ('wmic service SplunkForwarder get Pathname ^| findstr /m service') DO set SPLUNKPATH=%%i
+set SPLUNKPATH=%SPLUNKPATH:~1,-27%
+echo %DATE%-%TIME% The SplunkUniversalForwader is installed at %SPLUNKPATH%
 echo %DATE%-%TIME% Checking for Sysmon
 sc query "Sysmon" | Find /c "RUNNING" 1>nul && echo %DATE%-%TIME% Sysmon found, checking version && c:\windows\sysmon.exe | Find /c "System Monitor v6.20" 1>nul && echo %DATE%-%TIME% Sysmon already up to date, exiting && exit
 sc query "Sysmon" | Find /c "RUNNING" 1>nul && c:\windows\sysmon.exe | Find /c "System Monitor v6.20" >nul || echo %DATE%-%TIME% Sysmon binary is outdated, un-installing && c:\windows\sysmon -u
 echo %DATE%-%TIME% Sysmon not found, proceding to install
-echo %DATE%-%TIME% Copying the latest config file && copy /z /y "C:\Program Files\SplunkUniversalForwarder\etc\apps\TA-Sysmon-deploy\bin\config.xml" "C:\windows\" 
-echo %DATE%-%TIME% Installing Sysmon && "C:\Program Files\SplunkUniversalForwarder\etc\apps\TA-Sysmon-deploy\bin\sysmon.exe" /accepteula -i c:\windows\config.xml | Find /c "sysmon installed" 1>nul && echo %DATE%-%TIME% Install complete! && exit
+echo %DATE%-%TIME% Copying the latest config file && copy /z /y "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\config.xml" "C:\windows\" 
+echo %DATE%-%TIME% Installing Sysmon && "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\sysmon.exe" /accepteula -i c:\windows\config.xml | Find /c "sysmon installed" 1>nul && echo %DATE%-%TIME% Install complete! && exit
 echo %DATE%-%TIME% Install failed
 )
--- bin/update.bat
+++ bin/update.bat
@@ -1,6 +1,8 @@
 >> c:\windows\sysmon.log (
-fc c:\windows\config.xml "c:\Program Files\SplunkUniversalForwarder\etc\apps\TA-Sysmon-deploy\bin\config.xml" | Find "no differences" 1>nul && echo %DATE%-%TIME% No updates found && exit
-echo %DATE%-%TIME% Update found, installing && copy /z /y "C:\Program Files\SplunkUniversalForwarder\etc\apps\TA-Sysmon-deploy\bin\config.xml" "C:\windows\" && "C:\windows\sysmon.exe" -c c:\windows\config.xml
+FOR /F "delims=" %%i IN ('wmic service SplunkForwarder get Pathname ^| findstr /m service') DO set SPLUNKPATH=%%i
+set SPLUNKPATH=%SPLUNKPATH:~1,-27%
+fc c:\windows\config.xml "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\config.xml" | Find "no differences" 1>nul && echo %DATE%-%TIME% No updates found && exit
+echo %DATE%-%TIME% Update found, installing && copy /z /y "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\config.xml" "C:\windows\" && "C:\windows\sysmon.exe" -c c:\windows\config.xml
 )
 
 
