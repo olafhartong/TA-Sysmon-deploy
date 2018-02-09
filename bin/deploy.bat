304f62ba641ea0cfc49d46b2b751e238fffff362
Olaf Hartong <olafhartong@Olafs-MacBook-Pro.local>
2018-02-09 21:45:43 +0100

    added Splunkd restart

--- bin/deploy.bat
+++ bin/deploy.bat
@@ -1,13 +1,19 @@
 echo off
 FOR /F "delims=" %%i IN ('wmic service SplunkForwarder get Pathname ^| findstr /m service') DO set SPLUNKDPATH=%%i
 set SPLUNKPATH=%SPLUNKDPATH:~1,-28%
+
 >> c:\windows\sysmon.log (
 echo %DATE%-%TIME% The SplunkUniversalForwarder is installed at %SPLUNKPATH%
 echo %DATE%-%TIME% Checking for Sysmon
 sc query "Sysmon" | Find /c "RUNNING" 1>nul && echo %DATE%-%TIME% Sysmon found, checking version && c:\windows\sysmon.exe | Find /c "System Monitor v6.20" 1>nul && echo %DATE%-%TIME% Sysmon already up to date, exiting && exit
+
 sc query "Sysmon" | Find /c "RUNNING" 1>nul && c:\windows\sysmon.exe | Find /c "System Monitor v6.20" >nul || echo %DATE%-%TIME% Sysmon binary is outdated, un-installing && c:\windows\sysmon -u
+
 echo %DATE%-%TIME% Sysmon not found, proceding to install
 echo %DATE%-%TIME% Copying the latest config file && copy /z /y "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\config.xml" "C:\windows\" 
-echo %DATE%-%TIME% Installing Sysmon && "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\sysmon.exe" /accepteula -i c:\windows\config.xml | Find /c "sysmon installed" 1>nul && echo %DATE%-%TIME% Install complete! && exit
+echo %DATE%-%TIME% Installing Sysmon && "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\sysmon.exe" /accepteula -i c:\windows\config.xml | Find /c "sysmon installed" 1>nul && echo %DATE%-%TIME% Install complete! 
+
+sc query "Sysmon" | Find /c "RUNNING" 1>nul && "%SPLUNKPATH%\bin\splunk.exe" restart && exit
+
 echo %DATE%-%TIME% Install failed
 )
