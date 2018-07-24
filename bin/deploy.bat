f198305e9fcfbbbcd898e6c08ea75d8a84162b1f
Olaf Hartong <olafhartong@Olafs-MacBook-Pro.local>
2018-07-24 10:24:51 +0200

    Updated to Sysmon v8

--- bin/deploy.bat
+++ bin/deploy.bat
@@ -5,13 +5,13 @@ set SPLUNKPATH=%SPLUNKDPATH:~1,-28%
 >> c:\windows\sysmon.log (
 echo %DATE%-%TIME% The SplunkUniversalForwarder is installed at %SPLUNKPATH%
 echo %DATE%-%TIME% Checking for Sysmon
-sc query "Sysmon" | Find /c "RUNNING" 2>nul && echo %DATE%-%TIME% Sysmon found, checking version && c:\windows\sysmon.exe | Find /c "System Monitor v7.02" 1>nul && echo %DATE%-%TIME% Sysmon already up to date, exiting && exit
+sc query "Sysmon" | Find /c "RUNNING" 2>nul && echo %DATE%-%TIME% Sysmon found, checking version && c:\windows\sysmon.exe | Find /c "System Monitor v8.00" 1>nul && echo %DATE%-%TIME% Sysmon already up to date, exiting && exit
 
-sc query "Sysmon" | Find /c "RUNNING" 1>nul && c:\windows\sysmon.exe | Find /c "System Monitor v7.02" >nul || echo %DATE%-%TIME% Sysmon binary is outdated, un-installing && c:\windows\sysmon -u
+sc query "Sysmon" | Find /c "RUNNING" 1>nul && c:\windows\sysmon.exe | Find /c "System Monitor v8.00" >nul || echo %DATE%-%TIME% Sysmon binary is outdated, un-installing && c:\windows\sysmon -u
 
 echo %DATE%-%TIME% Sysmon not found, proceding to install
 echo %DATE%-%TIME% Copying the latest config file && copy /z /y "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\config.xml" "C:\windows\"
-echo %DATE%-%TIME% Installing Sysmon && "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\sysmon.exe" /accepteula -i c:\windows\config.xml | Find /c "sysmon installed" 1>nul && echo %DATE%-%TIME% Install complete! && exit
+echo %DATE%-%TIME% Installing Sysmon && "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\sysmon.exe" /accepteula -i c:\windows\config.xml | Find /c "Sysmon installed" 1>nul && echo %DATE%-%TIME% Install complete! && exit
 
 echo %DATE%-%TIME% Install failed
 )
