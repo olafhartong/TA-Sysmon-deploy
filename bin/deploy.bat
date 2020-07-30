eef85c93a50f16ad92d4e428f7074814c1609036
Karma1331 <61952449+Karma1331@users.noreply.github.com>
2020-07-30 12:15:31 -0500

    Various Updates
    1. Use 64-bit Sysmon instead of 32-bit
    2. Only CHECK_SYSMON_VERSION if it's running in the first place
    3. Wasn't accepting '/accepteula' changed it do a '-accepteula'
    4. Added hash checks & added n l flags to log modules & network logs

--- bin/deploy.bat
+++ bin/deploy.bat
@@ -1,37 +1,34 @@
-ECHO OFF
-
 FOR /F "delims=" %%i IN ('wmic service SplunkForwarder get Pathname ^| FINDSTR /m service') DO SET SPLUNKDPATH=%%i
 SET SPLUNKPATH=%SPLUNKDPATH:~1,-28%
 
 >> %WINDIR%\sysmon.log (
 ECHO %DATE%-%TIME% The SplunkUniversalForwarder is installed at %SPLUNKPATH%
-ECHO %DATE%-%TIME% Checking for Sysmon
+ECHO %DATE%-%TIME% Checking for Sysmon64
 
-FOR /F "delims=" %%c IN ('sc query "Sysmon" ^| FIND /c "RUNNING"') DO (
+FOR /F "delims=" %%c IN ('sc query "Sysmon64" ^| FIND /c "RUNNING"') DO (
     SET CHECK_SYSMON_RUNNIG=%%c
 )
 
-FOR /F "delims=" %%b IN ('c:\windows\sysmon.exe ^| FIND /c "System Monitor v11.10"') DO (
+IF "%CHECK_SYSMON_RUNNIG%" == "1" (
+    ECHO %DATE%-%TIME% Sysmon64 found, checking version
+    FOR /F "delims=" %%b IN ('c:\windows\Sysmon64.exe ^| FIND /c "System Monitor v11.10"') DO (
     SET CHECK_SYSMON_VERSION=%%b
-)
-
-if "%CHECK_SYSMON_RUNNIG%" == "1" (
-    ECHO %DATE%-%TIME% Sysmon found, checking version
+    )
     IF "%CHECK_SYSMON_VERSION%" == "1" (
-        ECHO %DATE%-%TIME% Sysmon already up to date, exiting
+        ECHO %DATE%-%TIME% Sysmon64 already up to date, exiting
         EXIT
     ) ELSE (
-        ECHO %DATE%-%TIME% Sysmon binary is outdated, un-installing
-        IF EXIST %WINDIR%\sysmon.exe (
-            %WINDIR%\sysmon.exe -u
+        ECHO %DATE%-%TIME% Sysmon64 binary is outdated, un-installing
+        IF EXIST %WINDIR%\Sysmon64.exe (
+            %WINDIR%\Sysmon64.exe -u
         )
     )
 ) ELSE (
-    ECHO %DATE%-%TIME% Sysmon not found, proceding to install
+    ECHO %DATE%-%TIME% Sysmon64 not found, proceding to install
     ECHO %DATE%-%TIME% Copying the latest config file
     COPY /z /y "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\config.xml" "C:\windows\"
-    ECHO %DATE%-%TIME% Installing Sysmon
-    "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\sysmon.exe" /accepteula -i c:\windows\config.xml | Find /c "Sysmon installed" 1>NUL
+    ECHO %DATE%-%TIME% Installing Sysmon64
+    "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\.\Sysmon64.exe" -i c:\windows\config.xml -accepteula -h md5,sha256 -n -l | Find /c "Sysmon64 installed" 1>NUL
     ECHO %DATE%-%TIME% Install complete!
     EXIT
 )
