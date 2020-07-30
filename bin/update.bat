7304e8e43affd519feb0cbd27a91abe88fbb675c
Karma1331 <61952449+Karma1331@users.noreply.github.com>
2020-07-30 12:18:28 -0500

    Use Sysmon 64 bit

--- bin/update.bat
+++ bin/update.bat
@@ -2,7 +2,5 @@ FOR /F "delims=" %%i IN ('wmic service SplunkForwarder get Pathname ^| findstr /
 set SPLUNKPATH=%SPLUNKDPATH:~1,-28%
 >> c:\windows\sysmon.log (
 fc c:\windows\config.xml "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\config.xml" | Find "no differences" 1>nul && echo %DATE%-%TIME% No updates found && exit
-echo %DATE%-%TIME% Update found, installing && copy /z /y "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\config.xml" "C:\windows\" && "C:\windows\sysmon.exe" -c c:\windows\config.xml
+echo %DATE%-%TIME% Update found, installing && copy /z /y "%SPLUNKPATH%\etc\apps\TA-Sysmon-deploy\bin\config.xml" "C:\windows\" && "C:\windows\Sysmon64.exe" -c c:\windows\config.xml
 )
-
-
